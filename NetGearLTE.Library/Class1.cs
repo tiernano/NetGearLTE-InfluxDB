63c54009c46574ad705c8918e6b58e2e03f9124b
Tiernan OToole <tiernan@tiernanotoole.ie>
2019-05-05 19:47:19 +0100

    splitting into library and main exe

---
+++ NetGearLTE.Library/Class1.cs
@@ -0,0 +1,52 @@
+﻿using Flurl.Http;
+using NLog;
+using System;
+using System.Text.RegularExpressions;
+
+namespace NetGearLTE.Library
+{
+    public class Client
+    {
+        private readonly Logger mLogger;
+        private readonly IFlurlClient mFlurlClient;
+
+        public Client(string BaseURL, string Password)
+        {
+            mLogger = LogManager.GetCurrentClassLogger();
+
+            mFlurlClient = new FlurlClient(BaseURL).EnableCookies().Configure(settings =>
+            {
+                settings.BeforeCall = call => mLogger.Debug($"Calling Netgear Url {call.Request.RequestUri}");
+                settings.OnError = call => mLogger.Error($"Error calling Netgear: {call.RequestBody} -  {call.Exception}");
+                settings.AfterCall = call =>
+                    mLogger.Debug($"called Netgear with {call.HttpStatus} in {call.Duration}");
+            });
+
+            string responce = mFlurlClient.Request().GetStringAsync().GetAwaiter().GetResult();
+
+            Regex regex = new Regex("name=\"token\" value=\"(.*?)\"");
+
+            Match match = regex.Match(responce);
+
+            if (match.Success)
+            {
+
+                string token = match.Groups[1].Value;
+                mLogger.Debug($"Got a match. found {token}");
+
+                var result = mFlurlClient.Request("Forms/config").SetQueryParam("session.password", Password).SetQueryParam("token", token)
+                    .GetAsync().GetAwaiter().GetResult();
+
+            }
+            else
+            {
+                throw new Exception("Did not get token from URL");
+            }
+        }
+
+        public Model GetModel()
+        {
+            return mFlurlClient.Request("/api/model.json").SetQueryParam("internalapi", "1").GetJsonAsync<Model>().GetAwaiter().GetResult();
+        }
+    }
+}
--- NetGearLTE/Program.cs
+++ NetGearLTE/Program.cs
@@ -19,98 +19,74 @@ namespace NetGearLTE
 
 
             string baseUrl = "http://192.168.5.1";
+            string password = "qL6rc4Eo";
 
-            var cli = new FlurlClient(baseUrl).EnableCookies().Configure(settings =>
+            NetGearLTE.Library.Client client = new Library.Client(baseUrl, password);
+
+           
+            mLogger.Debug("Calling out to telegraf");
+            Metrics.Collector = new CollectorConfiguration()
+                .Tag.With("host", Environment.GetEnvironmentVariable("COMPUTERNAME"))
+                .Batch.AtInterval(TimeSpan.FromSeconds(30))
+                .WriteTo.InfluxDB("http://192.168.1.119:8086", "telegraf")
+                .CreateCollector();               
+
+
+            while (true)
             {
-                settings.BeforeCall = call => mLogger.Debug($"Calling Netgear Url {call.Request.RequestUri}");
-                settings.OnError = call => mLogger.Error($"Error calling Netgear: {call.RequestBody} -  {call.Exception}");
-                settings.AfterCall = call =>
-                    mLogger.Debug($"called Netgear with {call.HttpStatus} in {call.Duration}");
-            });
-            
-            string responce = cli.Request().GetStringAsync().GetAwaiter().GetResult();
-
-            Regex regex = new Regex("name=\"token\" value=\"(.*?)\"");
-
-            Match match = regex.Match(responce);
-
-            if (match.Success)
-            {
-               
-                string token = match.Groups[1].Value;
-                string password = "qL6rc4Eo";
-                mLogger.Debug($"Got a match. found {token}");
-
-                var result = cli.Request("Forms/config").SetQueryParam("session.password", password).SetQueryParam("token",token)
-                    .GetAsync().GetAwaiter().GetResult();
-
-
-                mLogger.Debug("Calling out to telegraf");
-                Metrics.Collector = new CollectorConfiguration()
-                    .Tag.With("host", Environment.GetEnvironmentVariable("COMPUTERNAME"))
-                    .Batch.AtInterval(TimeSpan.FromSeconds(30))
-                    .WriteTo.InfluxDB("http://192.168.1.119:8086", "telegraf")
-                    .CreateCollector();               
-
-
-                while (true)
+                try
                 {
-                    try
-                    {
-                        var model = cli.Request("/api/model.json").SetQueryParam("internalapi", "1").GetJsonAsync<Model>().GetAwaiter().GetResult();
+                    var model = client.GetModel();
 
-                        mLogger.Debug($"WANIP: {model.wwan.IP} SMS count: {model.sms.msgCount} Unread: {model.sms.unreadMsgs}");
-                        mLogger.Debug($"model.wwan.dataTransferred: {model.wwan.dataTransferred} ");
-                        mLogger.Debug($"model.wwan.dataUsage.generic.billingCycleLimit: {model.wwan.dataUsage.generic.billingCycleLimit} ");
-                        mLogger.Debug($"model.wwan.dataUsage.generic.dataTransferred: {model.wwan.dataUsage.generic.dataTransferred} ");
-                        mLogger.Debug($"model.wwan.dataTransferredTx: {model.wwan.dataTransferredTx} ");
-                        mLogger.Debug($"model.wwan.dataTransferredRx: {model.wwan.dataTransferredRx} ");
+                    mLogger.Debug($"WANIP: {model.wwan.IP} SMS count: {model.sms.msgCount} Unread: {model.sms.unreadMsgs}");
+                    mLogger.Debug($"model.wwan.dataTransferred: {model.wwan.dataTransferred} ");
+                    mLogger.Debug($"model.wwan.dataUsage.generic.billingCycleLimit: {model.wwan.dataUsage.generic.billingCycleLimit} ");
+                    mLogger.Debug($"model.wwan.dataUsage.generic.dataTransferred: {model.wwan.dataUsage.generic.dataTransferred} ");
+                    mLogger.Debug($"model.wwan.dataTransferredTx: {model.wwan.dataTransferredTx} ");
+                    mLogger.Debug($"model.wwan.dataTransferredRx: {model.wwan.dataTransferredRx} ");
 
-                        long tx = long.Parse(model.wwan.dataTransferredTx);
-                        long rx = long.Parse(model.wwan.dataTransferredRx);
-                        long cycleRemander = long.Parse(model.wwan.dataUsage.generic.billingCycleRemainder);
+                    long tx = long.Parse(model.wwan.dataTransferredTx);
+                    long rx = long.Parse(model.wwan.dataTransferredRx);
+                    long cycleRemander = long.Parse(model.wwan.dataUsage.generic.billingCycleRemainder);
 
 
-                        Metrics.Write("ltedata",
-                            new Dictionary<string, object>
-                            {
-                            { "tx", tx },
-                            { "rx",  rx},
-                            { "total", model.wwan.dataTransferred },
-                            { "billingCycleLimit",model.wwan.dataUsage.generic.billingCycleLimit },
-                            { "dataTransferred", model.wwan.dataUsage.generic.dataTransferred },
-                            { "billingCycleRemainder", cycleRemander}
-                            });
-                        mLogger.Debug("Wrote Data Trasfer Data");
+                    Metrics.Write("ltedata",
+                        new Dictionary<string, object>
+                        {
+                        { "tx", tx },
+                        { "rx",  rx},
+                        { "total", model.wwan.dataTransferred },
+                        { "billingCycleLimit",model.wwan.dataUsage.generic.billingCycleLimit },
+                        { "dataTransferred", model.wwan.dataUsage.generic.dataTransferred },
+                        { "billingCycleRemainder", cycleRemander}
+                        });
+                    mLogger.Debug("Wrote Data Trasfer Data");
 
-                        Metrics.Write("signalStrength",
-                            new Dictionary<string, object>
-                            {
-                            { "rssi", model.wwan.signalStrength.rssi },
-                            { "bars", model.wwan.signalStrength.bars },
-                            {"rscp", model.wwan.signalStrength.rscp },
-                            {"ecio", model.wwan.signalStrength.ecio },
-                            {"rsrp", model.wwan.signalStrength.rsrp },
-                            {"rsrq", model.wwan.signalStrength.rsrq },
-                            {"sinr", model.wwan.signalStrength.sinr },
-                            {"quality", model.wwanadv.radioQuality }
-                            });
+                    Metrics.Write("signalStrength",
+                        new Dictionary<string, object>
+                        {
+                        { "rssi", model.wwan.signalStrength.rssi },
+                        { "bars", model.wwan.signalStrength.bars },
+                        {"rscp", model.wwan.signalStrength.rscp },
+                        {"ecio", model.wwan.signalStrength.ecio },
+                        {"rsrp", model.wwan.signalStrength.rsrp },
+                        {"rsrq", model.wwan.signalStrength.rsrq },
+                        {"sinr", model.wwan.signalStrength.sinr },
+                        {"quality", model.wwanadv.radioQuality }
+                        });
 
-                        mLogger.Debug("Wrote Signnal info");
+                    mLogger.Debug("Wrote Signnal info");
 
-                        mLogger.Debug("waiting 30 seconds");
-                    }
-                    catch(Exception ex)
-                    {
-                        mLogger.Error(ex, "Error when either talking to modem or writing to InfluxDB");
-                    }
-
-                    Thread.Sleep(30000);
-
+                    mLogger.Debug("waiting 30 seconds");
+                }
+                catch(Exception ex)
+                {
+                    mLogger.Error(ex, "Error when either talking to modem or writing to InfluxDB");
                 }
 
-            }
+                Thread.Sleep(30000);
 
+            }
         }
     }
 }
