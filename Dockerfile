af7d7e954d0ab9945f5f1e0b71ffdd1415afd4a5
Tiernan OToole <tiernan@tiernanotoole.ie>
2023-03-05 22:00:39 +0000

    inital updates

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mcr.microsoft.com/dotnet/core/sdk:2.2 AS build
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
 
 WORKDIR /app
 COPY NetGearLTE/*.csproj ./NetGearLTE/
@@ -19,7 +19,7 @@ COPY NetGearLTE.Library/. ./NetGearLTE.Library/
 WORKDIR /app/NetGearLTE
 RUN dotnet publish -c Release -o out
 
-FROM mcr.microsoft.com/dotnet/core/runtime:2.2 AS runtime
+FROM mcr.microsoft.com/dotnet/runtime:6.0 AS runtime
 WORKDIR /app
 COPY --from=build /app/NetGearLTE/out ./
 ENTRYPOINT ["dotnet", "NetGearLTE.dll"]
--- NetGearLTE.Library/Client.cs
+++ NetGearLTE.Library/Client.cs
@@ -17,20 +17,22 @@ namespace NetGearLTE.Library
             .WriteTo.Console()
             .CreateLogger();
 
+            var jar = new CookieJar();
 
-            mFlurlClient = new FlurlClient(BaseURL).EnableCookies().Configure(settings =>
+            mFlurlClient = new FlurlClient(BaseURL).Configure(settings =>
             {
                 settings.BeforeCall = call => Log.Logger.Debug($"Calling Netgear Url {call.Request.RequestUri}");
                 settings.OnError = call => Log.Logger.Error($"Error calling Netgear: {call.RequestBody} -  {call.Exception}");
                 settings.AfterCall = call =>
                     Log.Logger.Debug($"called Netgear with {call.HttpStatus} in {call.Duration}");
+                settings.WithCookies(out var jar);
             });
 
-            string responce = mFlurlClient.Request().GetStringAsync().GetAwaiter().GetResult();
+            string response = mFlurlClient.Request().GetStringAsync().GetAwaiter().GetResult();
 
             Regex regex = new Regex("name=\"token\" value=\"(.*?)\"");
 
-            Match match = regex.Match(responce);
+            Match match = regex.Match(response);
 
             if (match.Success)
             {
